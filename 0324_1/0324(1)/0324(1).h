cd228fb52c424021975020a2e9a7ae7783febf5a
1234bnmhjk <1871304618@qq.com>
2020-05-06 10:04:11 +0800

    第一题

---
+++ 0324_1/0324(1)/0324(1).h
@@ -0,0 +1,33 @@
+﻿
+// 0324(1).h: 0324(1) 应用程序的主头文件
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // 主符号
+
+
+// CMy03241App:
+// 有关此类的实现，请参阅 0324(1).cpp
+//
+
+class CMy03241App : public CWinApp
+{
+public:
+	CMy03241App() noexcept;
+
+
+// 重写
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// 实现
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMy03241App theApp;
---
+++ 0324_1/0324(1)/0324(1)View.h
@@ -0,0 +1,51 @@
+﻿
+// 0324(1)View.h: CMy03241View 类的接口
+//
+
+#pragma once
+
+
+class CMy03241View : public CView
+{
+protected: // 仅从序列化创建
+	CMy03241View() noexcept;
+	DECLARE_DYNCREATE(CMy03241View)
+
+// 特性
+public:
+	CMy03241Doc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+
+// 实现
+public:
+	virtual ~CMy03241View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnShowname();
+};
+
+#ifndef _DEBUG  // 0324(1)View.cpp 中的调试版本
+inline CMy03241Doc* CMy03241View::GetDocument() const
+   { return reinterpret_cast<CMy03241Doc*>(m_pDocument); }
+#endif
+
